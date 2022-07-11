73600c0e4d1bc94e0a2563a8f57a98d42e37a686
Calil Drissi <calil.drissi@gmail.com>
2022-07-11 13:47:40 +0000

    git initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import {Container } from 'react-bootstrap';
+
+
+
+const App = ()=>{
+  return (
+    <Container className="App">
+      <h1>let's begin!!</h1>
+    </Container>
+  );
+}
+
+export default App;
