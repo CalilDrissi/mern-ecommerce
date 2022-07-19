4c1dacb58b61d111bb561797171c0945109e0f2d
Calil Drissi <calil.drissi@gmail.com>
2022-07-19 16:30:06 +0000

    database connection setup complete

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express  from  "express"
 import dotenv   from  "dotenv" 
 import products from "./data/products.js"
+import connectDB from "./config/db.js"
+import colors from 'colors'
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
+
 
 app.get("/", (req, res) => {
   res.send("API is Running...");
