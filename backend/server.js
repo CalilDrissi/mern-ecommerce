c932bf6a612b0a303a7e817f7894e9f9a2f650d7
Calil Drissi <calil.drissi@gmail.com>
2022-07-19 12:20:32 +0000

    es6 module implemented for node

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const products = require('./data/products')
+import express  from  "express"
+import dotenv   from  "dotenv" 
+import products from "./data/products.js"
 
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res)=> {
-   res.send('API is Running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is Running...");
+});
 
-app.get('/api/products', (req, res)=> {
-   res.json(products)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id', (req, res)=> {
-    const product = products.find(p => p._id === req.params.id)
-   res.json(product)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log('server running on port 5000'))
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
