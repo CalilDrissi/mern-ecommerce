1daafd97ebb2cc07fd91b375648c229f8121db55
Calil Drissi <calil.drissi@gmail.com>
2022-07-19 11:53:16 +0000

    server setup complete

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
+
 
 const ProductScreen = () => {
   const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(()=> {
+    const fetchProduct = async () => {
+      const id = params.id;
+      const {data} = await axios.get(`/api/products/${id}`);
+      console.log(data)
+      setProduct(data)
+    }
+
+    fetchProduct();
+}, [])
+
+
+
+  
+ 
 
   return (
     <>
